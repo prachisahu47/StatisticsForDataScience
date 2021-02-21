ea583855043df94f229b94f1c3e0e59e06a6401d
prachisahu47 <cs20m047@smail.iitm.ac.in>
2021-02-21 14:48:34 +0530

    first commit

---
+++ Assignment1/Problem1/Question1.jl
@@ -0,0 +1,24 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f44f59e0-7349-11eb-2d6b-45fb88b08d40
+begin
+	N=2000
+	s=[sum([rand(Int32) for j in 1:i])/i for i in 1:N]
+end
+
+
+# ╔═╡ 03fb0a10-734a-11eb-0699-79465ba51682
+begin
+	using Plots
+	scatter(1:N,s,label=["mean"],xlabel="Number of integers choosen")
+	plot!(1:N,zeros(N),lw=2,label="The zero line")
+end
+
+
+# ╔═╡ Cell order:
+# ╠═f44f59e0-7349-11eb-2d6b-45fb88b08d40
+# ╠═03fb0a10-734a-11eb-0699-79465ba51682
