1c3642bdba97b76de8c9e30a077d3bda87e90143
prachisahu47 <cs20m047@smail.iitm.ac.in>
2021-02-21 16:29:03 +0530

    update

--- Assignment1/Problem2/Question2.jl
+++ Assignment1/Problem2/Question2.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.20
+# v0.12.21
 
 using Markdown
 using InteractiveUtils
@@ -9,12 +9,6 @@ begin
 	using Plots
 	pyplot()
 end
-
-# ╔═╡ 3509f8b0-71b4-11eb-1dfc-d15e3b817be8
-using Distributions
-
-# ╔═╡ 287b9d2e-71f8-11eb-1a10-0b5cf2ce7cf5
-using LinearAlgebra
 
 # ╔═╡ 9722c230-700f-11eb-2804-af2ebe7ee9f5
 md"### Creating a deck with jack,queen and king as 11,12 and 13 respectively"
@@ -85,18 +79,6 @@ begin
 	
 end
 
-# ╔═╡ a97d8ce2-71bc-11eb-03af-ef29c93f771a
-
-
-# ╔═╡ e1fefe60-71bb-11eb-0252-47a82dcd738a
-y=1
-
-# ╔═╡ 41899640-71b4-11eb-29fe-3b8fb9f7d5c8
-distBinom=[(binomial(5,i)*(0.077)^i*(0.923)^(5-i)) for i in 0:5]
-
-# ╔═╡ afa3d670-71ed-11eb-1c6d-f11c73963b3b
-distHyGeo=[(binomial(4,i)*binomial(48,5-i))/binomial(52,5) for i in 0:4]
-
 # ╔═╡ 76de0d40-705f-11eb-383e-ef93a5c3271b
 md"## _plotting together_"
 
@@ -106,24 +88,6 @@ begin
 	scatter!([0:5],prob,label=false)
 	plot!([0:4],prob_,xlabel="Number of jacks",label="without replacement",lcolor=:red)
 	plot!([0:5],prob,label="with replacement",lcolor=:blue)
-end
-
-# ╔═╡ 545b86b0-71f6-11eb-0d96-690550e2160c
-error1=distBinom-prob
-
-# ╔═╡ 593999a0-71f7-11eb-2a0c-551c65c5d879
-meanSqErr1=dot(error1,error1)/6
-
-# ╔═╡ b44a3df0-71f6-11eb-0448-adf25f408c60
-error2=distHyGeo-prob_
-
-# ╔═╡ 32118850-71f8-11eb-1b88-e36e60600330
-meanSqErr2=dot(error2,error2)/5
-
-# ╔═╡ c2518cf0-71f6-11eb-38e2-3bb63ec2b285
-begin
-	plot([0:5],error1,label="error in with replacement",markershape=:star5,xlabel="Number of jacks(n)")
-	plot!([0:4],error2,label="error in without replacement",markershape=:diamond,title="Difference in theoretical and experimental results")
 end
 
 # ╔═╡ Cell order:
@@ -139,16 +103,5 @@ end
 # ╠═a50fc840-7058-11eb-341a-8f1156193844
 # ╠═5ca368c0-705b-11eb-126a-fbeef234e024
 # ╠═7a446690-705b-11eb-1566-3158adda4c42
-# ╠═3509f8b0-71b4-11eb-1dfc-d15e3b817be8
-# ╠═a97d8ce2-71bc-11eb-03af-ef29c93f771a
-# ╠═e1fefe60-71bb-11eb-0252-47a82dcd738a
-# ╠═41899640-71b4-11eb-29fe-3b8fb9f7d5c8
-# ╠═afa3d670-71ed-11eb-1c6d-f11c73963b3b
 # ╟─76de0d40-705f-11eb-383e-ef93a5c3271b
 # ╠═08b696c0-705f-11eb-1efb-4d71bbc2bb4c
-# ╠═287b9d2e-71f8-11eb-1a10-0b5cf2ce7cf5
-# ╠═545b86b0-71f6-11eb-0d96-690550e2160c
-# ╠═593999a0-71f7-11eb-2a0c-551c65c5d879
-# ╠═32118850-71f8-11eb-1b88-e36e60600330
-# ╠═b44a3df0-71f6-11eb-0448-adf25f408c60
-# ╠═c2518cf0-71f6-11eb-38e2-3bb63ec2b285
